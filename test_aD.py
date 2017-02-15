3e9c11508274eb8944ef22c2bb4b048e6a693c01
Michalis Agathos <spxma1@raven14.bullx>
2017-02-15 01:46:16 +0000

    minor changes in test plots

--- test_aD.py
+++ test_aD.py
@@ -2,8 +2,8 @@
 #matplotlib.use('Agg')
 from pylab import *
 from LIV_postproc import *
-za = 0.1 + arange(20)*0.1
-alpha = 2.0
+za = 0.01 + arange(30)*0.01
+alpha = 0.0
 
 Da=[]
 Da0=[]
@@ -23,12 +23,13 @@ for z in za:
 
 fig = figure()
 ax = fig.add_subplot(111)
-ax.set_xlim((0.0, 2.0))
+ax.set_title("$\\alpha = " + str(alpha) + "$")
+ax.set_xlim((0.0, 0.3))
 ax.set_ylabel('cosmological distance [m]')
 ax.set_xlabel('redshift')
-ax.scatter(za,array(Da), color='g', label='$D_\\alpha$')
-ax.scatter(za,array(Da0), color='r', label='$D_\\alpha^\{LO\}$')
+ax.scatter(za,array(Da), color='g', label="$D_{\\alpha=" + str(alpha) + "}$")
+ax.scatter(za,array(Da0), color='r', label="$D_{\\alpha=" + str(alpha) + "}^{LO}$")
 ax.scatter(za,array(DL), color='b', label='$D_L$')
 ax.legend(loc='upper left', fancybox=True)
 
-fig.savefig("/home/spxma1/public_html/LVC/plot.png")
+fig.savefig("/home/spxma1/public_html/LVC/D-z_alpha0_small.png")
